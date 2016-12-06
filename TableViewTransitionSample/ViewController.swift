13f5676ec1503826ce2e993bf0d7df5f29867d67
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-12-06 23:53:59 +0900

    Initial Commit

---
+++ TableViewTransitionSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewTransitionSample
+//
+//  Created by Keisei Saito on 2016/12/06.
+//  Copyright © 2016 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
